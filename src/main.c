7654a6cc608d02542eaf3ac9c71f51377b4c6cae
Milena Aghabekyan <miaghabe@c2r1s2.42yerevan.am>
2025-06-29 17:41:59 +0400

    maini mej popoxutyun

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: miaghabe <miaghabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 15:34:28 by miaghabe          #+#    #+#             */
-/*   Updated: 2025/06/29 14:19:50 by miaghabe         ###   ########.fr       */
+/*   Updated: 2025/06/29 17:39:27 by miaghabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,18 @@ int	main(int argc, char **argv, char **env)
 		line = readline("minishell: ");
 		if (!line)
 			break;
+		if (line == NULL) // esi ctrl + D masna ibr bayc mi dzev srtovs chi
+		{
+			printf("exit\n");
+			rl_clear_history();
+			exit(0);
+		}
 		if (line && *line != '\0')
 		{
+			init_tokens(line, &data_base);
+			// print_tokens(data_base.token);
+			if (check_syntax_errors(data_base.token))
+				return (1);
 			cmd.args = ft_split(line, ' ');
 			cmd.next = NULL;
 			if (is_builtin(cmd.args[0]))
